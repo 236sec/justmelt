f16a9d4b5db1d80a3b7a65712eddaf23e6678046
ciaabcdefg <ciaabcdefg@gmail.com>
2024-04-15 00:38:30 +0700

    dash

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int maxHP = 50;
+
     public int currentHP;
+    public bool godMode = false;
+    
     //private SpriteRenderer sprite;
     // Start is called before the first frame update
     void Start()
@@ -27,6 +30,8 @@ public class PlayerHealth : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (godMode) return;
+
         BulletTag bTag = other.gameObject.GetComponent<BulletTag>();
         if (bTag != null)
         {
