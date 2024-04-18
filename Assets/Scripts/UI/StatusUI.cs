ed5ff0d5ccf4a1a7a2d0066177ded70e754ca05d
ciaabcdefg <ciaabcdefg@gmail.com>
2024-04-18 21:01:00 +0700

    ui

--- Assets/Scripts/UI/StatusUI.cs
+++ Assets/Scripts/UI/StatusUI.cs
@@ -8,19 +8,24 @@ public class StatusUI : MonoBehaviour
     [SerializeField] TextMeshProUGUI hpLabel;
     [SerializeField] TextMeshProUGUI parryLabel;
     [SerializeField] Bar hpBar;
+    [SerializeField] Bar parryBar;
 
     [SerializeField] PlayerHealth playerHealth; 
     [SerializeField] SwordParry swordParry; 
 
     void Update()
     {
-        hpLabel.text = $"HP: {playerHealth.currentHP}/{playerHealth.maxHP}";
+        hpLabel.text = $"{playerHealth.currentHP}/{playerHealth.maxHP}";
         hpBar.SetValue((float)playerHealth.currentHP / playerHealth.maxHP);
 
         if (swordParry.currentCooldownTime < 0) {
             parryLabel.text = $"0.00s";
-        } else {
+        } else if (swordParry.currentCooldownTime > 0) {
             parryLabel.text = $"{swordParry.currentCooldownTime:0.00}s";
+            parryBar.SetValue(swordParry.currentCooldownTime / swordParry.cooldownTime);
+        } else {
+            parryLabel.text = $"Ready";
+            parryBar.SetValue(1);
         }
     }
 }
